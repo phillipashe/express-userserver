5c3362fd06ded68dd3685346e8657abb92d518e6
phillipashe <ashephillip@gmail.com>
2022-07-20 13:10:31 -0400

    setting up sequelize

---
+++ models/users.js
@@ -0,0 +1,69 @@
+const {
+  DataTypes
+} = require('sequelize');
+
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: true,
+      field: "id",
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.CHAR(255),
+      allowNull: true,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "firstName",
+      autoIncrement: false
+    },
+    lastName: {
+      type: DataTypes.CHAR(255),
+      allowNull: true,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "lastName",
+      autoIncrement: false
+    },
+    emailName: {
+      type: DataTypes.CHAR(255),
+      allowNull: true,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "emailName",
+      autoIncrement: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "createdAt",
+      autoIncrement: false
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: null,
+      comment: null,
+      primaryKey: false,
+      field: "updatedAt",
+      autoIncrement: false
+    }
+  };
+  const options = {
+    tableName: "users",
+    comment: "",
+    indexes: []
+  };
+  const UsersModel = sequelize.define("users_model", attributes, options);
+  return UsersModel;
+};
